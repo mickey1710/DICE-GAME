293fe504a7b1d84982a56ab29a694f9d26c2a390
DIVYADHAR PANDEY <106006302+mickey1710@users.noreply.github.com>
2022-07-19 11:08:45 +0530

    Update dicee.js

--- dicee.js
+++ dicee.js
@@ -1,12 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-var randomDiceImage = "images/dice" + randomNumber1 + ".png"; 
+var randomDiceImage = "dice" + randomNumber1 + ".png"; 
 document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
 
 
 
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"; 
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; 
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 
 
